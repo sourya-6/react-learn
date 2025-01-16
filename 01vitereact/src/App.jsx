c60809e185f462963b13e7639f97374d69dadfa8
sourya-6 <saisourya678@gmail.com>
2025-01-16 15:54:51 +0530

    small return function done

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,13 +1,17 @@
 
-
+import Sourya from "./Sourya"
 
 
 function App() {
  
 
   return (
+    <>
    <h1>Sourya aur react</h1>
+   <Sourya/>
+   </>
   )
 }
-
+//We need to return the content in a single argument we can have multiple tags 
+// inside it so we using empty tag
 export default App
---
+++ 01vitereact/src/Sourya.jsx
@@ -0,0 +1,12 @@
+function Sourya(){
+    return(
+        <h1>Hey These course is good.</h1>
+        //We need to define the function name in first capital letter .
+        // if we are returning something related to html content mostly prefer having it in jsx
+        //note1000:-ante react components create chesetapudu jsx vadutamu
+    )
+    //js:- logic define ki
+    //jsx:- ui ni render ki
+}
+
+export default Sourya
