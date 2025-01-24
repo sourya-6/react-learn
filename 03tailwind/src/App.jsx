1fd086417c91bd5fd0f4bc90cbd9a8b4dda80fb1
sourya-6 <saisourya678@gmail.com>
2025-01-24 18:38:42 +0530

    tailwind components props learned

---
+++ 03tailwind/src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+import Card from './components/image'
+function App() {
+  //we can import the components from other files so we can reuse them just like we imported it from the image component
+  //here we can pass the parameters into the card component like object and array
+  // let obj={//remember that it needs to pass only via {} only same we can pass array to
+  //   name:"sourya",
+  //   age:20
+  // }
+
+  return (
+    <>
+      <h1 className='bg-green-500 p-4 m-4 rounded-xl'>Tailwind Test</h1>
+      <Card username="sourya"  />
+      <Card username="subodh" buttonText="Visit me" />
+      
+    </>
+  )
+}
+
+export default App
