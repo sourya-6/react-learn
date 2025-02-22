6c26fc6a9fbe5c701908ef287604f7d8f240aae9
sourya-6 <saisourya678@gmail.com>
2025-02-23 00:13:24 +0530

    added the toogle changer and started a project to know about the localStorage

--- 08contextApi/src/components/Login.jsx
+++ 08contextApi/src/components/Login.jsx
@@ -15,9 +15,12 @@ function Login() {
   return (
     <div >
         <h2 className=' m-20 text-3xl font-bold  '>Login Dashboard</h2>
+        
         <input type="text" className='m-1 border-1 border-black text-black bg-white px-2 py-1 rounded-xl m:2 ' value={username} onChange={(e)=>setUsername(e.target.value)} placeholder='username' />
+        <br />
         <input type="password"  className=' m-1 border-1 border-black text-black bg-white px-2 py-1 rounded-xl m:2' value={password} onChange={(e)=>setPassword(e.target.value)} placeholder='password' />
-        <button  className='border-2 border-white text-white bg-blue-600 px-3 py-1 rounded-xl' onClick={Submit}>Submit</button>
+        <br />
+        <button  className='border-2 border-green text-white bg-blue-600 px-3 py-1 m-10 rounded-xl' onClick={Submit}>Submit</button>
         <Profile className=''/>
     </div>
   )
