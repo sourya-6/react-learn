c48fd762dcd9f0d2a1709b67d6b00d370b22362e
sourya-6 <saisourya678@gmail.com>
2025-08-08 08:23:15 +0530

    changed home

--- 11MegaProject/src/components/PostCard.jsx
+++ 11MegaProject/src/components/PostCard.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 
 function PostCard({ $id, title, featuredImage }) {
     const [imageurl, setImageUrl] = useState('');
+    console.log("hey dude",featuredImage)
 
     useEffect(() => {
         const fetchImage = async () => {
